5f2c041133187956656b969c20d242d31f048f20
mahendrakumarsingh <150394885+mahendrakumarsingh@users.noreply.github.com>
2026-08-23 22:56:17 +0530

    Time: 8 ms (53.28%), Space: 46.9 MB (64.23%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = num.charAt(i);
+            if (i < n / 2) {
+                if (c == '?') leftQ++;
+                else leftSum += c - '0';
+            } else {
+                if (c == '?') rightQ++;
+                else rightSum += c - '0';
+            }
+        }
+        if ((leftQ + rightQ) % 2 == 1) return true;
+        int diff = leftSum - rightSum;
+        int qDiff = rightQ - leftQ;
+        return diff * 2 != qDiff * 9;
+    }
+}
